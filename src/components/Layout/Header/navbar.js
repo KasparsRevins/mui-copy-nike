05781ede4a7fdb7f3b628193ae29f707c2de4342
Kaspars Revins <okoikwedced@gmail.com>
2023-10-05 12:42:27 +0100

    Initial layout for first flexbox

--- src/components/Layout/Header/navbar.js
+++ src/components/Layout/Header/navbar.js
@@ -2,13 +2,11 @@ import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
-import { Button, Container, Hidden } from "@mui/material";
+import { Box, Button, Container, Hidden } from "@mui/material";
 
 function NikeNavbar() {
   const [menuAnchorEl, setMenuAnchorEl] = useState(null);
@@ -22,10 +20,36 @@ function NikeNavbar() {
   };
 
   return (
-    <AppBar sx={{ backgroundColor: "#F5F5F5", color: "black" }}>
-      <Container>
-        <Toolbar>
-          {/* <IconButton
+    <>
+      <AppBar sx={{ backgroundColor: "white", color: "black" }}>
+        <Box sx={{ backgroundColor: "#F5F5F5", display: "flex", justifyContent: 'flex-end'}}>
+          <IconButton sx={{ fontSize: 12}}>
+          <p>
+            Find a Store
+          </p>
+          </IconButton>
+            <IconButton sx={{ fontSize: 12}}>
+          <p>
+
+            Help
+          </p>
+            </IconButton>
+            <IconButton sx={{ fontSize: 12}}>
+          <p>
+
+            Join Us
+          </p>
+            </IconButton>
+            <IconButton sx={{ fontSize: 12}}>
+          <p>
+
+            Sign In
+          </p>
+            </IconButton>
+        </Box>
+        <Container>
+          <Toolbar>
+            {/* <IconButton
             edge="start"
             color="inherit"
             aria-label="menu"
@@ -33,35 +57,45 @@ function NikeNavbar() {
           >
             <MenuIcon />
           </IconButton> */}
-          <img src="https://cdn.iconscout.com/icon/free/png-512/free-nike-15-761696.png?f=webp&w=256" width={50} height={50}/>
-          <Hidden smDown>
-            <Button color="inherit">New & Featured</Button>
-            <Button color="inherit">Men</Button>
-            <Button color="inherit">Women</Button>
-            <Button color="inherit">Kids</Button>
-            <Button color="inherit">Sale</Button>
+            <IconButton>
+              <img
+                src="https://cdn.iconscout.com/icon/free/png-512/free-nike-15-761696.png?f=webp&w=256"
+                width={50}
+                height={50}
+              />
+            </IconButton>
+            <Hidden smDown>
+              <Button color="inherit">New & Featured</Button>
+              <Button color="inherit">Men</Button>
+              <Button color="inherit">Women</Button>
+              <Button color="inherit">Kids</Button>
+              <Button color="inherit">Sale</Button>
+            </Hidden>
+            <IconButton>
+              <FavoriteBorderIcon></FavoriteBorderIcon>
+            </IconButton>
+            <IconButton>
+              <WorkOutlineIcon></WorkOutlineIcon>
+            </IconButton>
+          </Toolbar>
+          <Hidden smUp>
+            <Menu
+              anchorEl={menuAnchorEl}
+              open={Boolean(menuAnchorEl)}
+              onClose={handleMenuClose}
+            >
+              <MenuItem>New & Featured</MenuItem>
+              <MenuItem>Men</MenuItem>
+              <MenuItem>Women</MenuItem>
+              <MenuItem>Kids</MenuItem>
+              <MenuItem>Sale</MenuItem>
+              <MenuItem>Join Us</MenuItem>
+              <MenuItem>Log In</MenuItem>
+            </Menu>
           </Hidden>
-          <FavoriteBorderIcon></FavoriteBorderIcon>
-          <WorkOutlineIcon></WorkOutlineIcon>
-
-        </Toolbar>
-        <Hidden smUp>
-          <Menu
-            anchorEl={menuAnchorEl}
-            open={Boolean(menuAnchorEl)}
-            onClose={handleMenuClose}
-          >
-            <MenuItem>New & Featured</MenuItem>
-            <MenuItem>Men</MenuItem>
-            <MenuItem>Women</MenuItem>
-            <MenuItem>Kids</MenuItem>
-            <MenuItem>Sale</MenuItem>
-            <MenuItem>Join Us</MenuItem>
-            <MenuItem>Log In</MenuItem>
-          </Menu>
-        </Hidden>
-      </Container>
-    </AppBar>
+        </Container>
+      </AppBar>
+    </>
   );
 }
 

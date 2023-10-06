357bb58b04aeefe75e070ee3bbf80c4eae432eba
Kaspars Revins <okoikwedced@gmail.com>
2023-10-06 11:42:09 +0100

    Layout improvements

--- src/components/Layout/Header/navbar.js
+++ src/components/Layout/Header/navbar.js
@@ -26,40 +26,44 @@ function NikeNavbar() {
           sx={{
             backgroundColor: "#F5F5F5",
             display: "flex",
-            justifyContent: "flex-end",
+            justifyContent: "space-between",
           }}
         >
-          <IconButton>
-            <img
-              src="https://upload.wikimedia.org/wikipedia/en/3/37/Jumpman_logo.svg"
-              width={50}
-              height={25}
-              alt="jordan"
-            />
-          </IconButton>
-          <IconButton>
-            <img
-              src="https://1000logos.net/wp-content/uploads/2021/04/Converse-logo.png"
-              width={50}
-              height={25}
-              alt="converse"
-            />
-          </IconButton>
-          <IconButton sx={{ fontSize: 12 }}>
-            <p>Find a Store</p>
-          </IconButton>
-          <IconButton sx={{ fontSize: 12 }}>
-            <p>Help</p>
-          </IconButton>
-          <IconButton sx={{ fontSize: 12 }}>
-            <p>Join Us</p>
-          </IconButton>
-          <IconButton sx={{ fontSize: 12 }}>
-            <p>Sign In</p>
-          </IconButton>
+          <div>
+            <IconButton>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/en/3/37/Jumpman_logo.svg"
+                width={50}
+                height={25}
+                alt="jordan"
+              />
+            </IconButton>
+            <IconButton>
+              <img
+                src="https://1000logos.net/wp-content/uploads/2021/04/Converse-logo.png"
+                width={50}
+                height={25}
+                alt="converse"
+              />
+            </IconButton>
+          </div>
+          <div>
+            <IconButton sx={{ fontSize: 12 }}>
+              <p>Find a Store</p>
+            </IconButton>
+            <IconButton sx={{ fontSize: 12 }}>
+              <p>Help</p>
+            </IconButton>
+            <IconButton sx={{ fontSize: 12 }}>
+              <p>Join Us</p>
+            </IconButton>
+            <IconButton sx={{ fontSize: 12 }}>
+              <p>Sign In</p>
+            </IconButton>
+          </div>
         </Box>
-        <Container>
-          <Toolbar sx={{ display: "flex", justifyContent: "center" }}>
+        <Box>
+          <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
             {/* <IconButton
             edge="start"
             color="inherit"
@@ -68,26 +72,32 @@ function NikeNavbar() {
           >
             <MenuIcon />
           </IconButton> */}
-            <IconButton>
-              <img
-                src="https://cdn.iconscout.com/icon/free/png-512/free-nike-15-761696.png?f=webp&w=256"
-                width={50}
-                height={50}
-              />
-            </IconButton>
-            <Hidden smDown>
-              <Button color="inherit">New & Featured</Button>
-              <Button color="inherit">Men</Button>
-              <Button color="inherit">Women</Button>
-              <Button color="inherit">Kids</Button>
-              <Button color="inherit">Sale</Button>
-            </Hidden>
-            <IconButton>
-              <FavoriteBorderIcon></FavoriteBorderIcon>
-            </IconButton>
-            <IconButton>
-              <WorkOutlineIcon></WorkOutlineIcon>
-            </IconButton>
+            <div>
+              <IconButton>
+                <img
+                  src="https://cdn.iconscout.com/icon/free/png-512/free-nike-15-761696.png?f=webp&w=256"
+                  width={50}
+                  height={50}
+                />
+              </IconButton>
+            </div>
+            <div>
+              <Hidden smDown>
+                <Button color="inherit">New & Featured</Button>
+                <Button color="inherit">Men</Button>
+                <Button color="inherit">Women</Button>
+                <Button color="inherit">Kids</Button>
+                <Button color="inherit">Sale</Button>
+              </Hidden>
+            </div>
+            <div>
+              <IconButton>
+                <FavoriteBorderIcon></FavoriteBorderIcon>
+              </IconButton>
+              <IconButton>
+                <WorkOutlineIcon></WorkOutlineIcon>
+              </IconButton>
+            </div>
           </Toolbar>
           <Hidden smUp>
             <Menu
@@ -104,7 +114,7 @@ function NikeNavbar() {
               <MenuItem>Log In</MenuItem>
             </Menu>
           </Hidden>
-        </Container>
+        </Box>
       </AppBar>
     </>
   );

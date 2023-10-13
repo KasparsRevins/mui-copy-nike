fa912cb401f139e66ac6873866322e2757da7909
Kaspars Revins <okoikwedced@gmail.com>
2023-10-13 11:55:27 +0100

    Carousel semi finished

--- src/components/Layout/Body/imagecarousel.js
+++ src/components/Layout/Body/imagecarousel.js
@@ -6,18 +6,18 @@ import "react-multi-carousel/lib/styles.css"; // You may need to import the CSS 
 const MyCarousel = () => {
   const responsive = {
     desktop: {
-      breakpoint: { max: 3000, min: 1024 },
+      breakpoint: { max: 3000, min: 1540 },
       items: 3,
       partialVisibilityGutter: 40,
     },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
+    tablet: {
+      breakpoint: { max: 1540, min: 1040 },
+      items: 2,
       partialVisibilityGutter: 30,
     },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
+    mobile: {
+      breakpoint: { max: 1040, min: 0 },
+      items: 1,
       partialVisibilityGutter: 30,
     },
   };
@@ -28,7 +28,7 @@ const MyCarousel = () => {
         additionalTransfrom={0}
         arrows
         autoPlaySpeed={3000}
-        centerMode={false}
+        centerMode={true}
         className=""
         containerClass="container-with-dots"
         dotListClass=""
@@ -52,32 +52,30 @@ const MyCarousel = () => {
         slidesToSlide={1}
         swipeable
       >
-        {/* Add your carousel items here */}
         <div>
-          <img src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_500,c_limit/26688ed0-7fa0-4a0b-915f-25b31d0152b0/nike-just-do-it.jpg" />
-          1
+          <img src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_500,c_limit/26688ed0-7fa0-4a0b-915f-25b31d0152b0/nike-just-do-it.jpg" alt="NikeApparel" style={{ maxWidth: "300px"}}/>
+          <p>Running Shoes</p>
         </div>
         <div>
-          <img src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_500,c_limit/5555c8b6-8872-4bcc-aac5-e15a9497f781/nike-just-do-it.jpg" />
-          2
+          <img src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_500,c_limit/5555c8b6-8872-4bcc-aac5-e15a9497f781/nike-just-do-it.jpg" alt="NikeApparel" style={{ maxWidth: "300px"}}/>
+          <p>Running Clothing</p>
         </div>
         <div>
-          <img src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_500,c_limit/ed96d4de-ea50-4e0e-98a6-5134aaf352c9/nike-just-do-it.jpg" />
-          3
+          <img src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_500,c_limit/ed96d4de-ea50-4e0e-98a6-5134aaf352c9/nike-just-do-it.jpg" alt="NikeApparel" style={{ maxWidth: "300px"}}/>
+          <p>Cold Weather</p>
         </div>
         <div>
-          <img src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_500,c_limit/9b13e501-3ffe-455b-a586-daa6efb859de/nike-just-do-it.jpg" />
-          4
+          <img src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_500,c_limit/9b13e501-3ffe-455b-a586-daa6efb859de/nike-just-do-it.jpg" alt="NikeApparel" style={{ maxWidth: "300px"}}/>
+          <p>Rainy Weather</p>
         </div>
         <div>
-          <img src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_500,c_limit/9ee65605-240c-4023-867b-5f3a40f73b7c/nike-just-do-it.jpg" />
-          5
+          <img src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_500,c_limit/9ee65605-240c-4023-867b-5f3a40f73b7c/nike-just-do-it.jpg" alt="NikeApparel" style={{ maxWidth: "300px"}}/>
+          <p>Jackets</p>
         </div>
         <div>
-          <img src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_500,c_limit/bf7333fb-8ecb-4394-bb0f-05b8a7cbb946/nike-just-do-it.jpg" />
-          6
+          <img src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_500,c_limit/bf7333fb-8ecb-4394-bb0f-05b8a7cbb946/nike-just-do-it.jpg" alt="NikeApparel" style={{ maxWidth: "300px"}}/>
+          <p>Trail Running</p>
         </div>
-        {/* Add more items as needed */}
       </Carousel>
     </Box>
   );

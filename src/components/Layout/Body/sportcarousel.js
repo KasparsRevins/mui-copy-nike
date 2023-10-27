69f787766a999653dacefe3d93a472faf5c30537
Kaspars Revins <okoikwedced@gmail.com>
2023-10-27 15:17:10 +0100

    Responsive carousels

--- src/components/Layout/Body/sportcarousel.js
+++ src/components/Layout/Body/sportcarousel.js
@@ -23,6 +23,65 @@ const SportCarousel = () => {
     },
   };
 
+  const sportItems = [
+    {
+      link: "/ComingSoon",
+      imgSrc:
+        "https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_300,c_limit/3aef76c8-46c5-4f2a-a18c-3e9bdc148433/nike-just-do-it.jpg",
+      alt: "NikeApparel",
+      title: "Running",
+    },
+    {
+      link: "/ComingSoon",
+      imgSrc:
+        "https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_300,c_limit/5027a28c-3984-4287-94d1-ee6198238ce9/nike-just-do-it.jpg",
+      alt: "NikeApparel",
+      title: "Football",
+    },
+    {
+      link: "/ComingSoon",
+      imgSrc:
+        "https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_300,c_limit/be13cfe4-b5a9-4c26-bee0-b716e98b099d/nike-just-do-it.jpg",
+      alt: "NikeApparel",
+      title: "Basketball",
+    },
+    {
+      link: "/ComingSoon",
+      imgSrc:
+        "https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_300,c_limit/b4722f0f-c0e4-4a1b-8050-9f026ef947e9/nike-just-do-it.jpg",
+      alt: "NikeApparel",
+      title: "Training",
+    },
+    {
+      link: "/ComingSoon",
+      imgSrc:
+        "https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_300,c_limit/5f432ab0-7ed1-48b9-b8c8-9eb3f5dcb63d/nike-just-do-it.jpg",
+      alt: "NikeApparel",
+      title: "Tennis",
+    },
+    {
+      link: "/ComingSoon",
+      imgSrc:
+        "https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_300,c_limit/11550514-c82b-46b2-afd8-e5dd62698371/nike-just-do-it.jpg",
+      alt: "NikeApparel",
+      title: "Dance",
+    },
+    {
+      link: "/ComingSoon",
+      imgSrc:
+        "https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_300,c_limit/ed6efa3d-bb62-4d41-89f1-49d01418fe49/nike-just-do-it.jpg",
+      alt: "NikeApparel",
+      title: "Yoga",
+    },
+    {
+      link: "/ComingSoon",
+      imgSrc:
+        "https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_300,c_limit/1d2fd0c4-a7dd-4159-84c0-a0364c0990e3/nike-just-do-it.jpg",
+      alt: "NikeApparel",
+      title: "Skateboarding",
+    },
+  ];
+
   return (
     <>
       <h3 style={{ marginLeft: "1em", marginRight: "1em" }}>Shop by Sport</h3>
@@ -55,110 +114,14 @@ const SportCarousel = () => {
           slidesToSlide={1}
           swipeable
         >
-          <div>
-            <Link
-              to="/ComingSoon"
-              style={{ textDecoration: "none", color: "grey" }}
-            >
-              <img
-                src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_300,c_limit/3aef76c8-46c5-4f2a-a18c-3e9bdc148433/nike-just-do-it.jpg"
-                alt="NikeApparel"
-                style={{ maxWidth: "300px" }}
-              />
-              <p>Running</p>
-            </Link>
-          </div>
-          <div>
-            <Link
-              to="/ComingSoon"
-              style={{ textDecoration: "none", color: "grey" }}
-            >
-              <img
-                src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_300,c_limit/5027a28c-3984-4287-94d1-ee6198238ce9/nike-just-do-it.jpg"
-                alt="NikeApparel"
-                style={{ maxWidth: "300px" }}
-              />
-              <p>Football</p>
-            </Link>
-          </div>
-          <div>
-            <Link
-              to="/ComingSoon"
-              style={{ textDecoration: "none", color: "grey" }}
-            >
-              <img
-                src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_300,c_limit/be13cfe4-b5a9-4c26-bee0-b716e98b099d/nike-just-do-it.jpg"
-                alt="NikeApparel"
-                style={{ maxWidth: "300px" }}
-              />
-              <p>Basketball</p>
-            </Link>
-          </div>
-          <div>
-            <Link
-              to="/ComingSoon"
-              style={{ textDecoration: "none", color: "grey" }}
-            >
-              <img
-                src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_300,c_limit/b4722f0f-c0e4-4a1b-8050-9f026ef947e9/nike-just-do-it.jpg"
-                alt="NikeApparel"
-                style={{ maxWidth: "300px" }}
-              />
-              <p>Training</p>
-            </Link>
-          </div>
-          <div>
-            <Link
-              to="/ComingSoon"
-              style={{ textDecoration: "none", color: "grey" }}
-            >
-              <img
-                src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_300,c_limit/5f432ab0-7ed1-48b9-b8c8-9eb3f5dcb63d/nike-just-do-it.jpg"
-                alt="NikeApparel"
-                style={{ maxWidth: "300px" }}
-              />
-              <p>Tennis</p>
-            </Link>
-          </div>
-          <div>
-            <Link
-              to="/ComingSoon"
-              style={{ textDecoration: "none", color: "grey" }}
-            >
-              <img
-                src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_300,c_limit/11550514-c82b-46b2-afd8-e5dd62698371/nike-just-do-it.jpg"
-                alt="NikeApparel"
-                style={{ maxWidth: "300px" }}
-              />
-              <p>Dance</p>
-            </Link>
-          </div>
-          <div>
-            <Link
-              to="/ComingSoon"
-              style={{ textDecoration: "none", color: "grey" }}
-            >
-              <img
-                src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_300,c_limit/ed6efa3d-bb62-4d41-89f1-49d01418fe49/nike-just-do-it.jpg"
-                alt="NikeApparel"
-                style={{ maxWidth: "300px" }}
-              />
-              <p>Yoga</p>
-            </Link>
-          </div>
-          <div>
-            <Link
-              to="/ComingSoon"
-              style={{ textDecoration: "none", color: "grey" }}
-            >
-              <img
-                src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_300,c_limit/1d2fd0c4-a7dd-4159-84c0-a0364c0990e3/nike-just-do-it.jpg"
-                alt="NikeApparel"
-                style={{ maxWidth: "300px" }}
-              />
-              <p>Skateboarding</p>
-            </Link>
-          </div>
+          {sportItems.map((item, index) => (
+            <div key={index}>
+              <Link to={item.link} style={{ textDecoration: "none", color: "grey" }}>
+                <img src={item.imgSrc} alt={item.alt} style={{ maxWidth: "95%" }} />
+                <p>{item.title}</p>
+              </Link>
+            </div>
+          ))}
         </Carousel>
       </Box>
     </>

5c3f5acc60739353ab0a44f3bda17811d061fe21
Kaspars Revins <okoikwedced@gmail.com>
2023-10-27 15:58:09 +0100

    Responsiveness

--- src/components/Layout/Body/Video/backgroundvideo.js
+++ src/components/Layout/Body/Video/backgroundvideo.js
@@ -1,22 +1,37 @@
 import React from "react";
 import ReactPlayer from "react-player";
 import { Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  paddingTop: "40px",
+  [theme.breakpoints.down("sm")]: {
+    height: "220px",
+  },
+  [theme.breakpoints.up("sm")]: {
+    height: "480px",
+  },
+  [theme.breakpoints.up("md")]: {
+    height: "720px",
+  },
+  [theme.breakpoints.up("lg")]: {
+    height: "1080px",
+  },
+}));
 
 export const BackgroundVideo = () => {
   return (
-    <>
-      <Box sx={{ paddingTop: "40px" }}>
-        <ReactPlayer
-          url="https://youtu.be/79hzoeRhP74?si=oeBYo8CqfmtNApxj"
-          muted="true"
-          playing="true"
-          loop="true"
-          width="100%"
-          height="1080px"
-          controls="false"
-        />
-      </Box>
-    </>
+    <StyledBox>
+      <ReactPlayer
+        url="https://youtu.be/79hzoeRhP74?si=oeBYo8CqfmtNApxj"
+        muted={true}
+        playing={true}
+        loop={true}
+        width="100%"
+        height="100%"
+        controls={false}
+      />
+    </StyledBox>
   );
 };
 
--- src/components/Layout/Body/explore.js
+++ src/components/Layout/Body/explore.js
@@ -1,7 +1,35 @@
 import { Box } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Explore() {
+  const sportItems = [
+    {
+      link: "/ComingSoon",
+      imgSrc:
+        "https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_540,c_limit/23fd8e65-c6a5-4b03-98e4-4a6281bd5592/nike-just-do-it.jpg",
+      alt: "explore-section",
+    },
+    {
+      link: "/ComingSoon",
+      imgSrc:
+        "https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_540,c_limit/df231f4d-b556-49d2-832d-368a167697c5/nike-just-do-it.jpg",
+      alt: "explore-section",
+    },
+    {
+      link: "/ComingSoon",
+      imgSrc:
+        "https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_540,c_limit/953a6d65-c55e-43e1-89d2-e5cb1630aba7/nike-just-do-it.jpg",
+      alt: "explore-section",
+    },
+    {
+      link: "/ComingSoon",
+      imgSrc:
+        "https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_540,c_limit/e6747725-2d8d-4338-b49b-bb680304b8d1/nike-just-do-it.jpg",
+      alt: "explore-section",
+    }
+  ];
+  
   return (
     <div>
       <div>
@@ -17,18 +45,14 @@ function Explore() {
             justifyContent: "space-evenly",
           }}
         >
-          <div style={{width: {xs: "5%", md: "10%", lg: "25%"} }}>
-            <img src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_540,c_limit/23fd8e65-c6a5-4b03-98e4-4a6281bd5592/nike-just-do-it.jpg" alt="explore-section"/>
-          </div>
-          <div style={{width: {xs: "5%", md: "10%", lg: "25%"}}}>
-            <img src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_540,c_limit/df231f4d-b556-49d2-832d-368a167697c5/nike-just-do-it.jpg" alt="explore-section"/>
-          </div>
-          <div style={{width: {xs: "5%", md: "10%", lg: "25%"}}}>
-            <img src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_540,c_limit/953a6d65-c55e-43e1-89d2-e5cb1630aba7/nike-just-do-it.jpg" alt="explore-section"/>
-          </div>
-          <div style={{width: {xs: "5%", md: "10%", lg: "25%"}}}>
-            <img src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/h_540,c_limit/e6747725-2d8d-4338-b49b-bb680304b8d1/nike-just-do-it.jpg" alt="explore-section"/>
-          </div>
+          {sportItems.map((item, index) => (
+            <div key={index}>
+              <Link to={item.link} style={{ textDecoration: "none", color: "grey" }}>
+                <img src={item.imgSrc} alt={item.alt} style={{ maxWidth: "95%" }} />
+                <p>{item.title}</p>
+              </Link>
+            </div>
+          ))}
         </Box>
       </div>
     </div>
